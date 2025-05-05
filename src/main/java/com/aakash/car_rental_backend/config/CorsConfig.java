b3e127a9cbbb5f83748f243962fb4880fbeb4ce9
aakashxyadav <iaakashyadavv@gmail.com>
2025-05-05 22:04:10 +0530

    add frontend origin

--- src/main/java/com/aakash/car_rental_backend/config/CorsConfig.java
+++ src/main/java/com/aakash/car_rental_backend/config/CorsConfig.java
@@ -8,13 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Value("${FRONTEND_URL}")
-    private String frontendurl;
+
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/**")
-                .allowedOrigins(frontendurl)
+                .allowedOrigins("https://swift-wheeels.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
